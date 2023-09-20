f36994907a62ff0a860cb7f919a1242ef53094f5
YuliiaTsaruk <julia.tsaruck@gmail.com>
2023-09-20 20:47:54 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LS_KEY = 'videoplayer-current-time';
+
+const onPlay = function (evt) {
+  //   console.log(evt.seconds);
+  const currentTime = evt.seconds;
+  localStorage.setItem(LS_KEY, JSON.stringify(currentTime));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedTime = JSON.parse(localStorage.getItem(LS_KEY));
+// console.log(savedTime);
+
+player.setCurrentTime(savedTime);
