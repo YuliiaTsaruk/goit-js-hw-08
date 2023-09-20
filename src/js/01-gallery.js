c0a633dd7ab07ff9959a8b2f9e3d18b248995990
YuliiaTsaruk <julia.tsaruck@gmail.com>
2023-09-20 18:53:29 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const listEl = document.querySelector('.gallery');
+const markup = createMarkup(galleryItems);
+
+listEl.insertAdjacentHTML('beforeend', markup);
+
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item" data-name="${description}">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  aptionPosition: 'bottom',
+  captionDelay: 250,
+});
